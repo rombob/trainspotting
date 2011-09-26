a0915feb0be470ae856399d0f9508156019d5d98
Oded Harth <odedharth@gmail.com>
2011-09-26 20:44:20 +0300

    first_meetup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @json = YAML.load_file(File.join(File.dirname(__FILE__),"../../db/stations.yml")).to_gmaps4rails
+    @json2 = YAML.load_file(File.join(File.dirname(__FILE__),"../../db/stations.yml")).to_json
+    
   end
 
 end
