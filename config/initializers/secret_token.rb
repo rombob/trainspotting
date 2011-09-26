b19d3a3af2090cd31c8077a139ae65c3fed2a067
Oded Harth <odedharth@gmail.com>
2011-09-26 20:05:36 +0300

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackthon::Application.config.secret_token = 'a8581b575e5ef317fa9fa7b7290203bfc0f002c6cf1289470ede3e8471a7743887fe40915fc75568ea878687d917649673e183137004013677e280de659ba482'
